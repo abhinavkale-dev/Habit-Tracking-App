38be86c894cf1d3cb90fa252dd1ff7cb8cc22f89
Abhi16016 <abhinavkale919913@gmail.com>
2025-02-02 00:14:46 +0530

    Zustand Project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import AddHabitForm from "@/components/habit-form";
 import HabitList from "@/components/habit-list";
 import HabitStats from "@/components/habit-stats";
 import useHabitStore from "@/store/store";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const App: React.FC = () => {
   const { fetchHabits } = useHabitStore();
@@ -14,18 +13,16 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="min-h-screen bg-background p-8 flex items-center justify-center">
-        <Card className="w-full max-w-2xl p-6 bg-card text-card-foreground">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8 text-center">
-            Habit Tracker
-          </h1>
-          <AddHabitForm />
-          <HabitList />
-          <HabitStats />
-        </Card>
-      </div>
-    </ThemeProvider>
+    <div className="min-h-screen bg-background p-8 flex items-center justify-center">
+      <Card className="w-full max-w-2xl p-6 bg-card text-card-foreground">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8 text-center">
+          Habit Tracker
+        </h1>
+        <AddHabitForm />
+        <HabitList />
+        <HabitStats />
+      </Card>
+    </div>
   );
 };
 
